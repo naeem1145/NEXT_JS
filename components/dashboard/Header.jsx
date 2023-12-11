6ebe5137d3f826fcdd216236ada3ffe2db90a12e
Naeem Hassan tonmoy <tonmoy.free10@gmail.com>
2023-12-11 18:58:16 +0600

    header left side work done

--- components/dashboard/Header.jsx
+++ components/dashboard/Header.jsx
@@ -1,12 +1,17 @@
+import { History } from 'lucide-react'
 import React from 'react'
+import SearchInput from './SearchInput'
 
 export default function Header() {
   return (
-    <div className='bg-slate-300'>
-      <div className='flex'>
+    <div className='bg-gray-100 h-12 flex items-center justify-between px-8'>
+      <div className='flex gap-3'>
         {/* Recent activities */}
-          <button></button>
+          <button >
+            <History className='w-6 h-6'/>
+          </button>
         {/* Search */}
+          <SearchInput/>
       </div>
     </div>
   )
