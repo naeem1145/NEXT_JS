b369fd0b368acedbd7dbed13d3cb6440c1eb7239
Naeem Hassan tonmoy <tonmoy.free10@gmail.com>
2023-12-11 18:18:56 +0600

    okafter problem fix npm run dev

---
+++ app/(back-office)/dashboard/home/overview/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <div className='text-xl mb-4'>Dashboard</div>
+  )
+}
---
+++ components/dashboard/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='bg-slate-300'>
+      <div className='flex'>
+        {/* Recent activities */}
+          <button></button>
+        {/* Search */}
+      </div>
+    </div>
+  )
+}
